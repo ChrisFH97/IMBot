61bf7bb771b1ef4a4db82b74ddd1908e13cb2ab7
Chris Haig <CHAIG200@Caledonian.ac.uk>
2019-06-24 22:55:50 +0100

    Regex and functions setup

---
+++ regex.js
@@ -0,0 +1,10 @@
+
+module.exports = { 
+    templates : {
+
+    url  : [/(http?(s):\/\/[^\\'\"<>]+?\\.(jpg|jpeg|gif|png))/g],
+    time : [/\d+\s*m[i]?[n]?[u]?[t]?[e]?[s]?/gi,/\d+\s*h[o]?[u]?[r]?[s]?/gi,/\d+\s*s[e]?[c]?[o]?[n]?[d]?[s]?/gi],
+    discordID  : /\d{18}/gi
+  
+  } 
+};
